b57c649e6a7109d4a541e0d05438eab47fe1ed06
WeeMick <michaelblr5@gmail.com>
2021-05-11 12:29:25 +0100

    chartjs added and chart showing in dashboard route

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class DashboardController extends AbstractController
+{
+
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(ChartBuilderInterface $chartBuilder)
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'Sales!',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [522, 1500, 2250, 2197, 2345, 3122, 3099],
+                ],
+            ],
+        ]);
+
+        return $this->render('dashboard/index.html.twig', [
+            'chart' => $chart
+        ]);
+    }
+}
